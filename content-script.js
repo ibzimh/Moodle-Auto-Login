e35ce1264aef0d21732e9e06a0b0fcf40a182326
ibzimh <ibzimh@gmail.com>
2023-09-07 11:51:42 -0400

    adding code

---
+++ content-script.js
@@ -0,0 +1,24 @@
+try {
+    // For Moodle
+    if (window.location.href.indexOf('umass.moonami.com') != -1) {
+        document.querySelectorAll("a[href='https://umass.moonami.com/login/index.php']")[0].click();
+    }
+    
+    // For Spire 
+    if (window.location.href.indexOf('umass.edu/spire/') != -1) {
+        document.querySelectorAll("a[href='https://www.spire.umass.edu/psp/heproda/?cmd=login']")[0].click();
+    }
+
+    // For Canvas (1)
+    if (window.location.href.indexOf('umamherst.instructure') != -1) {
+        document.querySelector("#global_nav_login_link").click();
+    }
+
+    // For Canvas (UMass IT)
+    if (window.location.href.indexOf('www.umass.edu/it') != -1) {
+        document.querySelectorAll("a[href='https://umamherst.instructure.com/login/saml']")[0].click();
+    }
+}
+catch(error) {
+    console.log("oops! there's an error logging in ", error);
+}
